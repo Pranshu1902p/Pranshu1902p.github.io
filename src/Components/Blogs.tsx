51af216e3ea75055cdb9c16454d317a875b5cdde
Pranshu Aggarwal <pranshu1902@gmail.com>
2022-12-15 23:19:26 +0530

    revert to previous build

--- src/Components/Blogs.tsx
+++ src/Components/Blogs.tsx
@@ -4,11 +4,6 @@ import Footer from "./Common/Footer";
 import Header from "./Common/Header";
 import data from "./blog.json";
 
-// import { PythonProvider } from "react-py";
-
-// https://www.npmjs.com/package/react-py
-// https://www.youtube.com/watch?v=VFLeF5Pu34A
-
 export default function Blogs() {
   const filters: linkType[] = [
     { title: "Home", active: false, link: "/" },
@@ -22,34 +17,9 @@ export default function Blogs() {
     document.title = "Blogs | Pranshu Aggarwal";
   });
 
-  // web scrapper python code
-  // const pythonCode = `
-  // print("data fetching started")
-  // import requests, bs4, json
-  // res = requests.get("https://medium.com/@pranshu1902")
-  // noStarchSoup = bs4.BeautifulSoup(res.content, 'html.parser')
-  // images = noStarchSoup.select("a > div > img")
-  // blogs = []
-  // n = 3
-  // end = n*2-1
-  // intros = noStarchSoup.select("a > div > p")
-  // data = noStarchSoup.select("a > div > img")
-
-  // for blog in range(0,end,2):
-  //     blogs.append({"title": data[blog].get("alt"), "image": data[blog].get("src"), "url": "https://medium.com"+data[blog].parent.parent.get("href"), "intro": intros[blog].text})
-
-  // with open("blog.json", "w") as f:
-  //     f.write("[")
-  //     for i in blogs:
-  //         f.write(json.dumps(i))
-  //         f.write(",")
-  //     f.write("]")
-  // `;
-
   return (
     <div className="absolute bg-gradient-to-br from-[#0a192f] via-[#0a192f] to-[#112240] text-[#c5c6c7] w-full min-h-screen">
       {/* Header */}
-      {/* <PythonProvider pythonCode={pythonCode}></PythonProvider> */}
       <Header filters={filters} />
       <div className="pt-6 pb-24 p-4 md:p-24 flex flex-col gap-4 w-full">
         <div style={{ paddingBottom: "3%" }} className="flex flex-row pt-6">
